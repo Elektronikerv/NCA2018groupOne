5860d85986ec61e26140ab8d82edcfa6971792bf
Vladyslav Kalischuk <elektronikerv@gmail.com>
2018-02-11 00:31:49 +0200

    Add DAO configuration

---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/config/DaoConfig.java
@@ -0,0 +1,45 @@
+package ncadvanced2018.groupeone.parent.config;
+
+import ncadvanced2018.groupeone.parent.dao.UserDao;
+import ncadvanced2018.groupeone.parent.dao.UserDaoImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+
+@Configuration
+@PropertySource(value = { "classpath:application.properties" })
+public class DaoConfig {
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+
+    @Bean
+    public UserDao getUserDao() {
+        return new UserDaoImpl(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+
+        return dataSource;
+    }
+}
