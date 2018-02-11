afabf16245de465371de9e88d33f9800ef7bf56d
Talakh <talax98@gmail.com>
2018-02-11 13:47:56 +0200

    Upd sql schema

--- backend/src/main/resources/database/keys.sql
+++ backend/src/main/resources/database/keys.sql
@@ -1,18 +1,26 @@
 ALTER TABLE "user_role"
-  ADD  FOREIGN  KEY ("role_fk") REFERENCES "role" ("role_pk");
+  ADD FOREIGN KEY ("role_fk") REFERENCES "role" ("role_pk");
 ALTER TABLE "user_role"
-  ADD  FOREIGN  KEY ("user_fk") REFERENCES "user" ("user_pk");
+  ADD FOREIGN KEY ("user_fk") REFERENCES "user" ("user_pk");
 ALTER TABLE "user"
-  ADD  FOREIGN  KEY ("manager") REFERENCES "user" ("user_pk");
+  ADD FOREIGN KEY ("manager") REFERENCES "user" ("user_pk");
+ALTER TABLE "user"
+  ADD FOREIGN KEY ("address_fk") REFERENCES "address" ("address_pk");
 ALTER TABLE "service"
-  ADD  FOREIGN  KEY ("order_fk") REFERENCES "order" ("order_pk");
+  ADD FOREIGN KEY ("order_fk") REFERENCES "order" ("order_pk");
 ALTER TABLE "service"
-  ADD  FOREIGN  KEY ("courier_fk") REFERENCES "user" ("user_pk");
+  ADD FOREIGN KEY ("courier_fk") REFERENCES "user" ("user_pk");
 ALTER TABLE "service"
-  ADD  FOREIGN  KEY ("operator_fk") REFERENCES "user" ("user_pk");
+  ADD FOREIGN KEY ("operator_fk") REFERENCES "user" ("user_pk");
 ALTER TABLE "order"
-  ADD  FOREIGN  KEY ("order_status_fk") REFERENCES "order_status" ("order_status_pk");
+  ADD FOREIGN KEY ("order_status_fk") REFERENCES "order_status" ("order_status_pk");
 ALTER TABLE "order"
-  ADD  FOREIGN  KEY ("office_fk") REFERENCES "office" ("office_pk");
+  ADD FOREIGN KEY ("office_fk") REFERENCES "office" ("office_pk");
 ALTER TABLE "order"
-  ADD  FOREIGN  KEY ("user_fk") REFERENCES "user" ("user_pk");
+  ADD FOREIGN KEY ("user_fk") REFERENCES "user" ("user_pk");
+ALTER TABLE "order"
+  ADD FOREIGN KEY (client_address_fk) REFERENCES "address" ("address_pk");
+ALTER TABLE "feedback"
+  ADD FOREIGN KEY ("order_fk") REFERENCES "order" ("order_pk");
+ALTER TABLE "office"
+  ADD FOREIGN KEY ("address_fk") REFERENCES "address" ("address_pk");
--- backend/src/main/resources/database/tables.sql
+++ backend/src/main/resources/database/tables.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS "feedback";
 DROP TABLE IF EXISTS "service";
 DROP TABLE IF EXISTS "order";
 DROP TABLE IF EXISTS "order_status";
@@ -5,6 +6,7 @@ DROP TABLE IF EXISTS "user_role";
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS "role";
 DROP TABLE IF EXISTS "office";
+DROP TABLE IF EXISTS "address";
 
 CREATE TABLE "role" (
   role_pk     SERIAL4 PRIMARY KEY  NOT NULL,
@@ -20,39 +22,50 @@ CREATE TABLE "user" (
   last_name    VARCHAR(45)                      NOT NULL,
   phone_number VARCHAR(45),
   email        VARCHAR(45) UNIQUE               NOT NULL,
-  manager      INT
+  manager      INT,
+  address_fk   INT
 );
 
 CREATE TABLE "user_role" (
   ur_pk   SERIAL4 PRIMARY KEY NOT NULL,
-  user_fk INT                NOT NULL,
-  role_fk INT                NOT NULL
+  user_fk INT                 NOT NULL,
+  role_fk INT                 NOT NULL
+);
+
+CREATE TABLE "address" (
+  address_pk SERIAL4 PRIMARY KEY  NOT NULL,
+  street     VARCHAR(45)          NOT NULL,
+  house      VARCHAR(5)           NOT NULL,
+  floor      SMALLINT             NOT NULL,
+  flat       SMALLINT             NOT NULL
 );
 
 CREATE TABLE "office" (
   office_pk   SERIAL4 PRIMARY KEY     NOT NULL,
-  name        VARCHAR(45)            NOT NULL,
-  street      VARCHAR(45)            NOT NULL,
-  house       SMALLINT,
-  floor       SMALLINT,
+  name        VARCHAR(45)             NOT NULL,
+  address_fk  INT                     NOT NULL,
   description VARCHAR(300)
 );
 
 CREATE TABLE "order" (
-  order_pk        SERIAL4 PRIMARY KEY    NOT NULL,
-  user_fk         INT                    NOT NULL,
-  office_fk       INT                    NOT NULL,
-  order_status_fk INT                    NOT NULL,
-  time            TIMESTAMP,
-  street          VARCHAR(45)            NOT NULL,
-  house           SMALLINT,
-  floor           SMALLINT,
-  description     VARCHAR(300)
+  order_pk          SERIAL4 PRIMARY KEY    NOT NULL,
+  user_fk           INT                    NOT NULL,
+  office_fk         INT                    NOT NULL,
+  order_status_fk   INT                    NOT NULL,
+  client_address_fk INT                    NOT NULL,
+  time              TIMESTAMP,
+  description       VARCHAR(300)
+);
+
+CREATE TABLE "feedback" (
+  feedback_pk SERIAL4 PRIMARY KEY  NOT NULL,
+  order_fk    INT                  NOT NULL,
+  text        VARCHAR(300)
 );
 
 CREATE TABLE "order_status" (
   order_status_pk SERIAL4 PRIMARY KEY    NOT NULL,
-  name            VARCHAR(45)  UNIQUE    NOT NULL,
+  name            VARCHAR(45) UNIQUE     NOT NULL,
   description     VARCHAR(300)
 );
 
@@ -60,5 +73,7 @@ CREATE TABLE "service" (
   service_pk  SERIAL4 PRIMARY KEY NOT NULL,
   order_fk    INT                 NOT NULL,
   courier_fk  INT,
-  operator_fk INT                 NOT NULL
+  operator_fk INT                 NOT NULL,
+  date        TIMESTAMP           NOT NULL,
+  attempt     SMALLINT            NOT NULL
 );
