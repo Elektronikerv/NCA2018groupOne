b4a4002675b6d7011b1883ccd15febab0a735714
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-02-12 23:37:48 +0200

    #1 added authorization security module

---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/authorization/config/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package ncadvanced2018.groupeone.parent.authorization.config;
+
+import ncadvanced2018.groupeone.parent.authorization.AuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationTokenFilter authenticationTokenFilter;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService, AuthenticationTokenFilter authenticationTokenFilter) {
+        super(true);
+        this.userDetailsService = userDetailsService;
+        this.authenticationTokenFilter = authenticationTokenFilter;
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+
+                .csrf().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .headers().cacheControl();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return this.userDetailsService;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public FilterRegistrationBean registration(AuthenticationTokenFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
+
+}
---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/authorization/model/AuthorizationResp.java
@@ -0,0 +1,11 @@
+package ncadvanced2018.groupeone.parent.authorization.model;
+
+public class AuthorizationResp {
+
+    private String token;
+
+    public AuthorizationResp(String token) {
+        this.token = token;
+    }
+
+}
