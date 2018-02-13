3fa6d050d77140d1c2e1f3e8595e1fe2f7a92e4f
Talakh <talax98@gmail.com>
2018-02-13 20:43:03 +0200

    Add NoSuchEntityException, bug fix

--- backend/src/main/java/ncadvanced2018/groupeone/parent/service/CrudService.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/service/CrudService.java
@@ -1,15 +1,18 @@
 package ncadvanced2018.groupeone.parent.service;
 
+import ncadvanced2018.groupeone.parent.exception.EntityExistsException;
+import ncadvanced2018.groupeone.parent.exception.NoSuchEntityException;
+
 import java.io.Serializable;
 import java.util.Optional;
 
 public interface CrudService<T, ID extends Serializable> {
 
-    T create(T entity);
+    T create(T entity) throws EntityExistsException;
 
-    T update(T entity);
+    T update(T entity) throws EntityExistsException;
 
-    Optional<T> findOne(ID id);
+    Optional<T> findOne(ID id) throws NoSuchEntityException;
 
     void delete(T entity);
 
