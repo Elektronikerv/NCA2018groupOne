cf8d7e62e0dccc778cf66ddd8d03d1d4079dc7a8
achernyak <chernyakalina.job@yandex.ua>
2018-02-17 20:50:57 +0200

    Added Data Source Config

---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/config/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package ncadvanced2018.groupeone.parent.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "hikariDataSource")
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig("/hikari.properties");
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return dataSource;
+    }
+}
