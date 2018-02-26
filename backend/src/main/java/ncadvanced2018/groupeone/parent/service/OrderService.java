00ac6c05ab57d8ed7304b79b334a44b3c03aa8d4
Vlad Kholodov <kholodov96@gmail.com>
2018-02-26 22:10:18 +0200

    Add OrderService

---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/service/OrderService.java
@@ -0,0 +1,17 @@
+package ncadvanced2018.groupeone.parent.service;
+
+import ncadvanced2018.groupeone.parent.model.entity.*;
+
+import java.util.List;
+
+public interface OrderService {
+    Order create(Order order);
+
+    Order findById(Long id);
+
+    Order update(Order order);
+
+    boolean delete(Order order);
+
+    boolean delete(Long id);
+}
