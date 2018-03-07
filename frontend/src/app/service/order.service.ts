d42d14c083d4a11153fb8a004c812648dedc1264
Vladyslav Kalischuk <elektronikerv@gmail.com>
2018-03-07 11:11:36 +0200

    Update order.service.ts

--- frontend/src/app/service/order.service.ts
+++ frontend/src/app/service/order.service.ts
@@ -3,16 +3,36 @@ import {Order} from "../model/order.model";
 import {Observable} from "rxjs/Observable";
 import {HttpClient} from "@angular/common/http";
 import {TokenService} from "./token.service";
+import { FulfillmentOrder } from '../model/fulfillmentOrder.model';
+import { User } from '../model/user.model';
 
 const url = '/api/orders';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private http: HttpClient, private tokenService: TokenService<Order>) { }
+  constructor(private http: HttpClient,
+    private tokenService: TokenService<Order>,
+    private fulfilmentTokenService : TokenService<FulfillmentOrder>) { }
 
   getOrderById(id: number): Observable<Order> {
     return this.tokenService.get(`${url}/${id}`);
+  }
+
+  getFulfillmentOrderById(id: number): Observable<FulfillmentOrder> {
+    return this.tokenService.get(`${url}/fo/${id}`);
+  }
+
+  getAllCouriers(): Observable<User[]> {
+    return this.tokenService.get(`${url}/couriers`);
+  }
+
+  createFulfillmentOrder(ccagentId: number, orderId: number): Observable<number> {
+    return this.tokenService.get(`${url}/fo/${ccagentId}/${orderId}`);
+  }
+
+  updateFullfimentOrder(fulfillmentOrder : FulfillmentOrder): Observable<FulfillmentOrder> {
+    return this.fulfilmentTokenService.put(`${url}/fo/${fulfillmentOrder.id}`, fulfillmentOrder);
   }
 
   getOrders(): Observable<Order[]> {
@@ -23,9 +43,6 @@ export class OrderService {
     return this.tokenService.put(`${url}/${order.id}`, order);
   }
 
-  create(order: Order): Observable<Order> {
-    console.log("Order service: create order");
-    return this.tokenService.post(url, order);
-  }
+
 
 }
