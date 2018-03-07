317a88b5ed1cc66e63052453544217d6b759e6a1
Nainek <Nainek95@gmail.com>
2018-03-07 14:49:10 +0200

    Refactoring

--- backend/src/main/java/ncadvanced2018/groupeone/parent/model/entity/WorkingDay.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/model/entity/WorkingDay.java
@@ -1,7 +1,12 @@
 package ncadvanced2018.groupeone.parent.model.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import ncadvanced2018.groupeone.parent.model.entity.impl.RealWorkingDay;
+
 import java.time.LocalDateTime;
 
+
+@JsonDeserialize(as = RealWorkingDay.class)
 public interface WorkingDay {
 
     Long getId();
