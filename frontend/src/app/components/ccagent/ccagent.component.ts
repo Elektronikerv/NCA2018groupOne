60c9d40f5be3a31f181ef920336efdaa9a72a2e4
Vladyslav Kalischuk <elektronikerv@gmail.com>
2018-03-07 11:11:13 +0200

    Update ccagent.component.ts

--- frontend/src/app/components/ccagent/ccagent.component.ts
+++ frontend/src/app/components/ccagent/ccagent.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from "../../service/order.service";
 import { Order } from "../../model/order.model";
-
+import {JwtHelper} from "angular2-jwt";
+import { FulfillmentOrder } from '../../model/fulfillmentOrder.model';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-ccagent',
   templateUrl: './ccagent.component.html',
@@ -10,8 +12,9 @@ import { Order } from "../../model/order.model";
 export class CcagentComponent implements OnInit {
 
   orders: Order[];
+  private JwtHelper: JwtHelper = new JwtHelper();
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
     this.getOrders();
@@ -21,4 +24,14 @@ export class CcagentComponent implements OnInit {
       console.log('getOrders()');
       this.orderService.getOrders().subscribe((orders: Order[]) => this.orders = orders);
   }
+
+  redirectToDetails(orderId: number) {
+      let token = localStorage.getItem("currentUser");
+      let ccagentId = +this.JwtHelper.decodeToken(token).id;
+      this.orderService.createFulfillmentOrder(ccagentId, orderId)
+      .subscribe((id :number) => { 
+        this.router.navigate([`ccagent/orders/${id}`]);
+        console.log("full order : " + JSON.stringify(id));
+      });
+    }
 }
