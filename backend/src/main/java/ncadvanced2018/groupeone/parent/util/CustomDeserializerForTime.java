ae265fe012246fc9720f85c6ecbdd949049e2df0
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-03-17 22:25:26 +0200

    fixed custom deserializer for time

--- backend/src/main/java/ncadvanced2018/groupeone/parent/util/CustomDeserializerForTime.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/util/CustomDeserializerForTime.java
@@ -16,17 +16,55 @@ public class CustomDeserializerForTime extends JsonDeserializer<LocalDateTime> {
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
 
         JsonNode root = jsonParser.getCodec().readTree(jsonParser);
-
         String receiverAvailabilityTime;
 
         receiverAvailabilityTime = root.asText();
+        if (root.toString().startsWith("[")) {
+            int i = 0;
+            String year = root.toString().substring(1, 5);
+
+            //root.toString() have format [yyyy,MM,dd,HH,mm,ss]
+            String month = root.toString().substring(6, 8);
+            if (month.endsWith(",")) {
+                i += 1;
+                month = root.toString().substring(6, 8 - i);
+                StringBuilder stringBuilder = new StringBuilder(month);
+                stringBuilder.setCharAt(0, '0');
+                month = stringBuilder.toString() + month;
+            }
+            String day = root.toString().substring(9 - i, 11 - i);
+            if (day.endsWith(",")) {
+                i += 1;
+                day = root.toString().substring(9 - i, 11 - i);
+                StringBuilder stringBuilder = new StringBuilder(day);
+                stringBuilder.setCharAt(0, '0');
+                day = stringBuilder.toString() + day;
+            }
+            String hour = root.toString().substring(12 - i, 14 - i);
+            if (hour.endsWith(",")) {
+                i += 1;
+                hour = root.toString().substring(12 - i, 14 - i);
+                StringBuilder stringBuilder = new StringBuilder(hour);
+                stringBuilder.setCharAt(0, '0');
+                hour = stringBuilder.toString() + hour;
+            }
+            String minutes = root.toString().substring(15 - i, 17 - i);
+            if (minutes.endsWith(",") || minutes.endsWith("]")) {
+                i += 1;
+                minutes = root.toString().substring(15 - i, 17 - i);
+                StringBuilder stringBuilder = new StringBuilder(minutes);
+                stringBuilder.setCharAt(0, '0');
+                minutes = stringBuilder.toString();
+            }
+
+            receiverAvailabilityTime = year + "-" + month + "-" + day + "T" + hour + ":" + minutes;
+        }
 
         String newTime = receiverAvailabilityTime + ":00";
 
         DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 
-        LocalDateTime time = LocalDateTime.parse(newTime);
-
-        return time;
+        return LocalDateTime.parse(newTime);
     }
+
 }
