646d88dae51496b4ff49274af02d1a87d462d995
Vlad Kholodov <kholodov96@gmail.com>
2018-03-18 08:11:01 +0200

    Add controller for Working Days

---
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/controller/WorkingDayController.java
@@ -0,0 +1,57 @@
+package ncadvanced2018.groupeone.parent.controller;
+
+import ncadvanced2018.groupeone.parent.model.entity.WorkingDay;
+import ncadvanced2018.groupeone.parent.service.WorkingDayService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/schedule")
+public class WorkingDayController {
+
+    private WorkingDayService workingDayService;
+
+    public WorkingDayController(WorkingDayService workingDayService){
+        this.workingDayService = workingDayService;
+    }
+
+    @PostMapping("create/{id}")
+    public ResponseEntity<WorkingDay> createWorkingDay(@RequestBody WorkingDay workingDay){
+        WorkingDay createdWorkingDay = workingDayService.create(workingDay);
+        return new ResponseEntity<>(createdWorkingDay, HttpStatus.CREATED);
+    }
+
+    @GetMapping("days/{id}")
+    public ResponseEntity<WorkingDay> getWorkingDayById(@PathVariable Long id){
+        WorkingDay wDay = workingDayService.findById(id);
+        return new ResponseEntity<>(wDay, HttpStatus.OK);
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<WorkingDay>> getAll(){
+        List<WorkingDay> wDays = workingDayService.getAll();
+        return new ResponseEntity<>(wDays, HttpStatus.OK);
+    }
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<List<WorkingDay>> getWorkingDaysByUserId(@PathVariable Long id){
+        List<WorkingDay> wDays = workingDayService.findByUserId(id);
+        return new ResponseEntity<>(wDays, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WorkingDay> updateWorkingDay(@RequestBody WorkingDay workingDay){
+        WorkingDay wDay = workingDayService.update(workingDay);
+        return new ResponseEntity<>(wDay, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteWorkingDay(@PathVariable Long id){
+        workingDayService.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
