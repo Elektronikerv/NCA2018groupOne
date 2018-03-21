aab5f5aa6147ce1aff8d4709534aab47b5d7d864
Talakh <talax98@gmail.com>
2018-03-21 15:23:22 +0200

    Google map for EditCCOrderCCAgent

--- frontend/src/app/components/ccagent/edit-c-c-order-ccagent/edit-c-c-order-ccagent.component.ts
+++ frontend/src/app/components/ccagent/edit-c-c-order-ccagent/edit-c-c-order-ccagent.component.ts
@@ -1,17 +1,15 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {FormBuilder, FormGroup, Validators, FormControl} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {OrderService} from "../../../service/order.service";
 import {Order} from "../../../model/order.model";
-import {OfficeService} from "../../../service/office.service";
-import {Office} from '../../../model/office.model';
-import {OrderStatus} from '../../../model/orderStatus.model';
 import {CustomValidators} from "ng2-validation";
-import {ORDER_STATUSES} from '../../../model/orderStatus.model';
 import {FulfillmentOrder} from '../../../model/fulfillmentOrder.model';
 import {User} from '../../../model/user.model';
 import {Address} from "../../../model/address.model";
 import {FLAT_PATTERN, FLOOR_PATTERN} from "../../../model/utils";
+import {GoogleMapsComponent} from "../../utils/google-maps/google-maps.component";
+import {MapsAPILoader} from "@agm/core";
 
 @Component({
   selector: 'app-edit-c-c-order-ccagent',
@@ -26,32 +24,45 @@ export class EditCCOrderCcagentComponent implements OnInit {
   receiverAddress: FormGroup;
   officeId: number;
 
+  mapSender: GoogleMapsComponent;
+  mapReceiver: GoogleMapsComponent;
+
+  @ViewChild('searchSenderAddress')
+  public searchSenderAddressRef: ElementRef;
+  @ViewChild('searchReceiverAddress')
+  public searchReceiverAddressToRef: ElementRef;
+
   constructor(private orderService: OrderService,
               private activatedRouter: ActivatedRoute,
               private router: Router,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private mapsAPILoader: MapsAPILoader,
+              private ngZone: NgZone) {
     this.fulfillmentOrder.order = <Order>{};
     this.fulfillmentOrder.order.user = <User>{};
     this.fulfillmentOrder.order.senderAddress = <Address>{};
     this.fulfillmentOrder.order.receiverAddress = <Address>{};
 
-
+    this.mapSender = new GoogleMapsComponent(mapsAPILoader, ngZone);
+    this.mapReceiver = new GoogleMapsComponent(mapsAPILoader, ngZone);
   }
 
   ngOnInit(): void {
+    this.mapSender.setSearchElement(this.searchSenderAddressRef);
+    this.mapSender.ngOnInit();
+    this.mapReceiver.setSearchElement(this.searchReceiverAddressToRef);
+    this.mapReceiver.ngOnInit();
     this.getFulfillmentOrder();
-
-
   }
 
   initForm(fulfillment: FulfillmentOrder) {
     this.orderForm = this.formBuilder.group({
-        senderAddress : this.initSenderAddress(),
+        senderAddress: this.initSenderAddress(),
         receiverAddress: this.initReceiverAddress(),
         description: new FormControl(CustomValidators.required),
-        receiverAvailabilityDate:['',[Validators.required]],
-        receiverAvailabilityFrom: ['',[Validators.required]],
-        receiverAvailabilityTo: ['',[Validators.required]],
+        receiverAvailabilityDate: ['', [Validators.required]],
+        receiverAvailabilityFrom: ['', [Validators.required]],
+        receiverAvailabilityTo: ['', [Validators.required]],
         receiverAvailabilityTimeFrom: new FormControl(),
         receiverAvailabilityTimeTo: new FormControl()
       }
@@ -59,7 +70,7 @@ export class EditCCOrderCcagentComponent implements OnInit {
   }
 
   initReceiverAddress(): FormGroup {
-    return  this.receiverAddress =this.formBuilder.group({
+    return this.receiverAddress = this.formBuilder.group({
       street: ['', [Validators.required, Validators.minLength(5)]],
       house: ['', [Validators.required, Validators.maxLength(5)]],
       floor: [0, [Validators.required, Validators.pattern(FLOOR_PATTERN)]],
@@ -68,7 +79,7 @@ export class EditCCOrderCcagentComponent implements OnInit {
   }
 
   initSenderAddress(): FormGroup {
-    return  this.senderAddress =this.formBuilder.group({
+    return this.senderAddress = this.formBuilder.group({
       street: ['', [Validators.required, Validators.minLength(5)]],
       house: ['', [Validators.required, Validators.maxLength(5)]],
       floor: [0, [Validators.required, Validators.pattern(FLOOR_PATTERN)]],
@@ -77,21 +88,19 @@ export class EditCCOrderCcagentComponent implements OnInit {
   }
 
 
-
-
   getFulfillmentOrder() {
     const id = +this.activatedRouter.snapshot.paramMap.get('id');
     this.orderService.getFulfillmentOrderById(id)
       .subscribe((order: FulfillmentOrder) => {
         this.fulfillmentOrder = order;
-        this.fulfillmentOrder.order.receiverAvailabilityDate = this.fulfillmentOrder.order.receiverAvailabilityTimeTo.toString().substring(0,10);
-        this.fulfillmentOrder.order.receiverAvailabilityFrom = this.fulfillmentOrder.order.receiverAvailabilityTimeFrom.toString().substring(11,16);
-        this.fulfillmentOrder.order.receiverAvailabilityTo = this.fulfillmentOrder.order.receiverAvailabilityTimeTo.toString().substring(11,16);
+        this.fulfillmentOrder.order.receiverAvailabilityDate = this.fulfillmentOrder.order.receiverAvailabilityTimeTo.toString().substring(0, 10);
+        this.fulfillmentOrder.order.receiverAvailabilityFrom = this.fulfillmentOrder.order.receiverAvailabilityTimeFrom.toString().substring(11, 16);
+        this.fulfillmentOrder.order.receiverAvailabilityTo = this.fulfillmentOrder.order.receiverAvailabilityTimeTo.toString().substring(11, 16);
         this.initForm(order);
       });
   }
 
-  confirmOrder(order : Order) {
+  confirmOrder(order: Order) {
     this.updateAvailabilityTime();
     this.orderService.confirmFulfillmentOrder(this.fulfillmentOrder)
       .subscribe(_ => this.router.navigate(['ccagent/orders']));
@@ -102,7 +111,7 @@ export class EditCCOrderCcagentComponent implements OnInit {
       .subscribe(_ => this.router.navigate(['ccagent/orders']))
   }
 
-  save(order : Order){
+  save(order: Order) {
     this.updateAvailabilityTime();
     this.update();
   }
@@ -112,13 +121,14 @@ export class EditCCOrderCcagentComponent implements OnInit {
       .subscribe(_ => this.router.navigate(['ccagent/orders']));
   }
 
-  private updateAvailabilityTime(){
-    console.log('From ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom );
-    console.log('TO ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom );
-    this.fulfillmentOrder.order.receiverAvailabilityTimeFrom =  this.fulfillmentOrder.order.receiverAvailabilityDate + ' ' + this.fulfillmentOrder.order.receiverAvailabilityFrom + ':00';
-    this.fulfillmentOrder.order.receiverAvailabilityTimeTo =  this.fulfillmentOrder.order.receiverAvailabilityDate + ' ' + this.fulfillmentOrder.order.receiverAvailabilityTo + ':00';
-    console.log('From ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom );
-    console.log('TO ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom ); }
+  private updateAvailabilityTime() {
+    console.log('From ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom);
+    console.log('TO ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom);
+    this.fulfillmentOrder.order.receiverAvailabilityTimeFrom = this.fulfillmentOrder.order.receiverAvailabilityDate + ' ' + this.fulfillmentOrder.order.receiverAvailabilityFrom + ':00';
+    this.fulfillmentOrder.order.receiverAvailabilityTimeTo = this.fulfillmentOrder.order.receiverAvailabilityDate + ' ' + this.fulfillmentOrder.order.receiverAvailabilityTo + ':00';
+    console.log('From ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom);
+    console.log('TO ' + this.fulfillmentOrder.order.receiverAvailabilityTimeFrom);
+  }
 
 
   validateFieldSenderAddress(field: string): boolean {
@@ -127,6 +137,46 @@ export class EditCCOrderCcagentComponent implements OnInit {
 
   validateFieldReceiverAddress(field: string): boolean {
     return this.receiverAddress.get(field).valid || !this.receiverAddress.get(field).dirty;
+  }
+
+  updateSenderStreet() {
+    this.fulfillmentOrder.order.senderAddress.street = this.mapSender.street;
+  }
+
+  updateSenderHouse() {
+    this.fulfillmentOrder.order.senderAddress.house = this.mapSender.house;
+  }
+
+  updateSenderStreetHouse() {
+    setTimeout(() => {
+      this.fulfillmentOrder.order.senderAddress.street = this.mapSender.street;
+      this.fulfillmentOrder.order.senderAddress.house = this.mapSender.house;
+    }, 500);
+  }
+
+  mapSenderReady($event, yourLocation) {
+    this.mapSender.mapReady($event, yourLocation);
+    this.mapSender.geocodeAddress(this.fulfillmentOrder.order.senderAddress.street, this.fulfillmentOrder.order.senderAddress.house);
+  }
+
+  updateReceiverStreet() {
+    this.fulfillmentOrder.order.receiverAddress.street = this.mapReceiver.street;
+  }
+
+  updateReceiverHouse() {
+    this.fulfillmentOrder.order.receiverAddress.house = this.mapReceiver.house;
+  }
+
+  updateReceiverStreetHouse() {
+    setTimeout(() => {
+      this.fulfillmentOrder.order.receiverAddress.street = this.mapReceiver.street;
+      this.fulfillmentOrder.order.receiverAddress.house = this.mapReceiver.house;
+    }, 500);
+  }
+
+  mapReceiverReady($event, yourLocation) {
+    this.mapReceiver.mapReady($event, yourLocation);
+    this.mapReceiver.geocodeAddress(this.fulfillmentOrder.order.receiverAddress.street, this.fulfillmentOrder.order.receiverAddress.house);
   }
 
 }
