db238afd901d4ae190a520d17d8574ee91367422
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-03-26 14:53:35 +0300

    fixed view of time, refactored sql

--- backend/src/main/java/ncadvanced2018/groupeone/parent/dto/OrderStatistic.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/dto/OrderStatistic.java
@@ -1,5 +1,6 @@
 package ncadvanced2018.groupeone.parent.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalTime;
@@ -11,8 +12,9 @@ public class OrderStatistic {
     private Long processedCCA;
     private Long processedCourier;
     private Long cancelledOrders;
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime avgTime;
-
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime delayTime;
     private Double lvlOfService;
     private Double cancelledPercent;
