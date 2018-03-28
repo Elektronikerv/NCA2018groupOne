82322b20e00e022445725b9a0e0b6004c9a9f78b
Vitaliy Semchyshyn <sem4yshyn.new@gmail.com>
2018-03-28 22:24:09 +0300

    Add variable google.api.key in profiles

--- backend/src/main/java/ncadvanced2018/groupeone/parent/config/GoogleAPIConfig.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/config/GoogleAPIConfig.java
@@ -4,13 +4,11 @@ import com.google.maps.GeoApiContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:maps.properties")
 public class GoogleAPIConfig {
 
-    @Value("${api.key}")
+    @Value("${google.api.key}")
     String apiKey;
 
     @Bean
