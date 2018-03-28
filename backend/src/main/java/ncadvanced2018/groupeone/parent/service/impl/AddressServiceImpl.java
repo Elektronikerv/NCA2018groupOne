6089885efacd2ce009763b421f4e57010cd500b8
achernyak <chernyakalina.job@yandex.ua>
2018-03-28 12:47:46 +0300

    refactoring Address Service

--- backend/src/main/java/ncadvanced2018/groupeone/parent/service/impl/AddressServiceImpl.java
+++ backend/src/main/java/ncadvanced2018/groupeone/parent/service/impl/AddressServiceImpl.java
@@ -3,14 +3,25 @@ package ncadvanced2018.groupeone.parent.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import ncadvanced2018.groupeone.parent.model.entity.Address;
 import ncadvanced2018.groupeone.parent.service.AddressService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@PropertySource("classpath:address.properties")
 public class AddressServiceImpl implements AddressService {
+
+    @Value("${city.location}")
+    private String cityLocation;
 
     @Override
     public String map(Address address) {
-        return address.getStreet() + ", " + address.getHouse() + ", " + "Kyiv, Ukraine";
+        return new StringBuilder()
+                .append(address.getStreet())
+                .append(", ")
+                .append(address.getHouse())
+                .append(", ")
+                .append(cityLocation).toString();
     }
 }
